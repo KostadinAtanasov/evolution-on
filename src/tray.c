1f455718b457cb253099cc1d3ffde67ea207ef25
Edwin Stang <gsubes@gmail.com>
2010-02-28 16:41:28 +0200

    merge support for hidden on startup via gconf and conf widget

--- src/tray.c
+++ src/tray.c
@@ -40,6 +40,9 @@
 #include <e-util/e-icon-factory.h>
 #include <shell/es-event.h>
 
+#define GCONF_KEY_ROOT                 "/apps/evolution/eplugin/tray/"
+#define GCONF_KEY_HIDDEN_ON_STARTUP GCONF_KEY_ROOT "hidden-on-startup"
+
 #if EVOLUTION_VERSION < 22900
 struct __EShellPrivate {
 	/* IID for registering the object on OAF.  */
@@ -62,8 +65,71 @@ GtkWidget *evo_window;
 #else
 EShellWindow *evo_window;
 #endif
-
 GtkStatusIcon *tray_icon = NULL;
+
+/****************** Configuration *************************/
+
+
+//Query GConf
+static gboolean
+is_part_enabled (const gchar *gconf_key)
+{
+	/* the part is not enabled by default */
+	gboolean res = FALSE;
+	GConfClient *client;
+	GConfValue  *is_key;
+
+	client = gconf_client_get_default ();
+
+	is_key = gconf_client_get (client, gconf_key, NULL);
+	if (is_key) {
+		res = gconf_client_get_bool (client, gconf_key, NULL);
+		gconf_value_free (is_key);
+	}
+
+	g_object_unref (client);
+
+	return res;
+}
+
+//Set GConf
+static void
+set_part_enabled (const gchar *gconf_key, gboolean enable)
+{
+	GConfClient *client;
+
+	client = gconf_client_get_default ();
+
+	gconf_client_set_bool (client, gconf_key, enable, NULL);
+	g_object_unref (client);
+}
+
+//Callback for Configuration Widget
+static void
+toggled_hidden_on_startup_cb (GtkWidget *widget, gpointer data)
+{
+	g_return_if_fail (widget != NULL);
+	set_part_enabled (GCONF_KEY_HIDDEN_ON_STARTUP, gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (widget)));
+}
+
+//Configuration Widget
+static GtkWidget *
+get_cfg_widget (void)
+{
+	GtkWidget *cfg, *vbox, *check;
+
+	vbox = gtk_vbox_new (FALSE, 6);
+	check = gtk_check_button_new_with_mnemonic (_("Hidden on startup"));
+
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check), is_part_enabled (GCONF_KEY_HIDDEN_ON_STARTUP));
+	g_signal_connect (G_OBJECT (check), "toggled", G_CALLBACK (toggled_hidden_on_startup_cb), NULL);
+	gtk_widget_show (check);
+	gtk_box_pack_start (GTK_BOX (vbox), check, FALSE, FALSE, 0);
+
+	gtk_widget_show (vbox);
+
+	return vbox;
+}
 
 void gtkut_window_popup(GtkWidget *window)
 {
@@ -81,7 +147,9 @@ void gtkut_window_popup(GtkWidget *window)
 	if (new_x != x || new_y != y)
 		gdk_window_move(window->window, new_x, new_y);
 
-	gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window), FALSE);
+	gtk_window_set_skip_taskbar_hint(
+		GTK_WINDOW(window),
+		FALSE);
 	gtk_window_present(GTK_WINDOW(window));
 #ifdef G_OS_WIN32
 	/* ensure that the window is displayed at the top */
@@ -89,15 +157,16 @@ void gtkut_window_popup(GtkWidget *window)
 #endif
 }
 
+//helper method for toggling used on init for hidden on startup and on tray click
 static void
-icon_activated (GtkStatusIcon *icon, gpointer pnotify)
+toggle_window (int just_minimize)
 {
 	GList *p, *pnext;
 #if EVOLUTION_VERSION < 22900
 	for (p = (gpointer)evo_window; p != NULL; p = pnext) {
 		pnext = p->next;
 
-		if (gtk_window_is_active(GTK_WINDOW(p->data))) {
+		if (just_minimize || gtk_window_is_active(GTK_WINDOW(p->data))) {
 			gtk_window_iconify(GTK_WINDOW(p->data));
 			gtk_window_set_skip_taskbar_hint(GTK_WINDOW(p->data), TRUE);
 		} else {
@@ -107,15 +176,21 @@ icon_activated (GtkStatusIcon *icon, gpointer pnotify)
 		}
 	}
 #else
-		if (gtk_window_is_active(evo_window)) {
-			gtk_window_iconify(evo_window);
-			gtk_window_set_skip_taskbar_hint(evo_window, TRUE);
-		} else {
-			gtk_window_iconify(evo_window);
-			gtkut_window_popup(evo_window);
-			gtk_window_set_skip_taskbar_hint(evo_window, FALSE);
-		}
+	if (gtk_window_is_active(GTK_WINDOW(evo_window))) {
+		gtk_window_iconify(GTK_WINDOW(evo_window));
+		gtk_window_set_skip_taskbar_hint(GTK_WINDOW(evo_window), TRUE);
+	} else {
+		gtk_window_iconify(GTK_WINDOW(evo_window));
+		gtkut_window_popup(GTK_WIDGET(evo_window));
+		gtk_window_set_skip_taskbar_hint(GTK_WINDOW(evo_window), FALSE);
+	}
 #endif
+}
+
+static void
+icon_activated (GtkStatusIcon *icon, gpointer pnotify)
+{
+	toggle_window(FALSE);
 }
 
 static void
@@ -125,7 +200,9 @@ create_status_icon(void)
 		tray_icon = gtk_status_icon_new ();
 		gtk_status_icon_set_from_pixbuf (
 			tray_icon, 
-			e_icon_factory_get_icon ("mail-send-receive", GTK_ICON_SIZE_SMALL_TOOLBAR));
+			e_icon_factory_get_icon (
+				"mail-send-receive",
+				GTK_ICON_SIZE_SMALL_TOOLBAR));
 		g_signal_connect (
 			G_OBJECT (tray_icon),
 			"activate",
@@ -135,14 +212,29 @@ create_status_icon(void)
 	gtk_status_icon_set_visible (tray_icon, TRUE);
 }
 
+GtkWidget *
+e_plugin_lib_get_configure_widget (EPlugin *epl);
+
 #if EVOLUTION_VERSION < 22900
-void org_gnome_evolution_tray_startup(void *ep, EMPopupTargetSelect *t);
+void
+org_gnome_evolution_tray_startup(
+		void *ep,
+		EMPopupTargetSelect *t);
 
-void org_gnome_evolution_tray_startup(void *ep, EMPopupTargetSelect *t)
+void
+org_gnome_evolution_tray_startup(
+		void *ep,
+		EMPopupTargetSelect *t)
 #else
-void org_gnome_evolution_tray_startup(void *ep, ESEventTargetUpgrade *t);
+void
+org_gnome_evolution_tray_startup(
+		void *ep,
+		ESEventTargetUpgrade *t);
 
-void org_gnome_evolution_tray_startup(void *ep, ESEventTargetUpgrade *t)
+void
+org_gnome_evolution_tray_startup(
+		void *ep,
+		ESEventTargetUpgrade *t)
 #endif
 {
 	g_print("Evolution-tray plugin enabled.\n");
@@ -157,6 +249,9 @@ void get_shell(void *ep, ESEventTargetShell *t)
 	EShellPrivate *priv = (EShellPrivate *)shell->priv;
 	evo_window = (GtkWidget *)priv->windows;
 #endif
+	if(is_part_enabled(GCONF_KEY_HIDDEN_ON_STARTUP)){
+		toggle_window(TRUE);
+	}
 }
 
 #if EVOLUTION_VERSION >= 22900
@@ -169,3 +264,10 @@ e_plugin_ui_init (
 	return TRUE;
 }
 #endif
+
+GtkWidget *
+e_plugin_lib_get_configure_widget (EPlugin *epl)
+{
+	return get_cfg_widget ();
+}
+
